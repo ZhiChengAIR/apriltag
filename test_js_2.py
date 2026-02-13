b2a8d08647b67de7c99fc2ad9e8ef1e10c5730fa
William <1337842463@qq.com>
2026-02-13 16:00:57 +0800

    quick debug

--- test_js_2.py
+++ test_js_2.py
@@ -1,5 +1,3 @@
-import time
-
 from apriltag_tracker import AprilTagPoseTracker
 
 tracker = AprilTagPoseTracker(
@@ -10,30 +8,10 @@ tracker = AprilTagPoseTracker(
       resolution="medium",
   )
 
-def wait_for_pose(tracker, timeout_s=5.0, frame="robot", warmup_s=0.8, poll_sleep_s=0.01):
-    """Wait until a pose is detected or timeout expires."""
-    start = time.time()
-    warmup_deadline = start + warmup_s
-
-    # Warm up camera/exposure
-    while time.time() < warmup_deadline:
-        tracker.get_tag_pose(frame=frame)
-
-    # Poll until detected or timeout
-    while time.time() - start < timeout_s:
-        pose = tracker.get_tag_pose(frame=frame)
+try:
+    while True:
+        pose = tracker.get_tag_pose(frame="robot")
         if pose:
-            return pose
-        if poll_sleep_s:
-            time.sleep(poll_sleep_s)
-
-    return None
-
-
-pose = wait_for_pose(tracker, timeout_s=5.0, frame="robot")
-if pose:
-    print(pose["position_mm"], pose["roll"], pose["pitch"], pose["yaw"])
-else:
-    print("No AprilTag detected (timeout).")
-
-tracker.close()
+            print(pose["position_mm"], pose["roll"], pose["pitch"], pose["yaw"])
+finally:
+    tracker.close()
